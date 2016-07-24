769cf687b53032e9e30d0d1f26968f23973e993d
lubien <lubien1996@gmail.com>
2016-07-24 14:28:11 -0300

    Implement Container

--- src/container.js
+++ src/container.js
@@ -1,2 +1,32 @@
-export default class Container {}
+export default class Container {
+	/*
+	 * @param {Service} service
+	 */
+	static register(service) {
+		const name = service.service;
+
+		if (name) {
+			Container._services[name] = service;
+			return true;
+		}
+
+		return false;
+	}
+
+	/*
+	 * @param {string} name
+	 */
+	static has(name) {
+		return {}.hasOwnProperty.call(Container._services, name);
+	}
+
+	/*
+	 * @param {string} name
+	 */
+	static get(name) {
+		return Container._services[name] || null;
+	}
+}
+
+Container._services = {};
 
