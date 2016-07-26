502c845a361aee97ff6a4af7bc07b5ab71ae2073
lubien <lubien1996@gmail.com>
2016-07-26 02:40:10 -0300

    Add request methods to Resource

--- src/resource.js
+++ src/resource.js
@@ -1,3 +1,4 @@
+import Request from './request';
 import {Container} from './';
 
 export default class Resource {
@@ -40,6 +41,103 @@ export default class Resource {
 	 */
 	getIdentifier() {
 		return this.attributes[this.identifierName] || null;
+	}
+
+	/**
+	 * @returns {Promise}
+	 */
+	static all() {
+		const instance = new this();
+		return instance.getAll();
+	}
+
+	/**
+	 * @returns {Resource[]}
+	 */
+	getAll() {
+		return this.makeRequest('get')
+			.then(response => {
+				const resources = this.factoryCollection(response);
+
+				if (this.hasParentResource()) {
+					return resources
+						.map(resource => {
+							resource.parentResource = this.parentResource;
+
+							return resource;
+						});
+				}
+
+				return resources;
+			});
+	}
+
+	/**
+	 * @param {string} identifier
+	 * @returns {Promise}
+	 */
+	static find(identifier = '') {
+		const instance = new this();
+		return instance.doFind(identifier);
+	}
+
+	/**
+	 * @param {string} identifier
+	 * @returns {Promise}
+	 */
+	doFind(identifier = '') {
+		this.attributes[this.identifierName] = identifier;
+
+		return this.makeRequest('get', true)
+			.then(response => {
+				this.factory(response);
+
+				return this;
+			});
+	}
+
+	/**
+	 * @returns {Promise}
+	 */
+	save() {
+		return this.makeRequest('post')
+			.then(response => {
+				this.factory(response);
+
+				return this;
+			});
+	}
+
+	/**
+	 * @returns {Promise}
+	 */
+	update() {
+		return this.makeRequest('put', true)
+			.then(response => {
+				this.factory(response);
+
+				return this;
+			});
+	}
+
+	/**
+	 * @returns {Promise}
+	 */
+	destroy() {
+		return this.makeRequest('delete', true);
+	}
+
+	/**
+	 * @param {string} method
+	 * @param {bool} identified
+	 * @param {?string} append
+	 * @param {?Object} customBody
+	 * @returns {Promise}
+	 */
+	makeRequest(method, identified = false, append = null, customBody = null) {
+		const request = new Request(this, identified, append, customBody);
+
+		return request.send(method);
 	}
 }
 
