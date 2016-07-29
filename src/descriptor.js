3f8badb8dcec10147f4ef371db128db56067db13
lubien <lubien1996@gmail.com>
2016-07-29 13:45:20 -0300

    Remove dead code

--- src/descriptor.js
+++ src/descriptor.js
@@ -5,11 +5,10 @@ export default class Descriptor {
 	/**
 	 * @param {Object} params
 	 */
-	constructor({service, headers, prefix, agent}) {
+	constructor({service, prefix, agent}) {
 		debug(`Creating Descriptor ${service}`);
 
 		this.service = service;
-		this.headers = headers;
 		this.prefix = prefix;
 		this.agent = agent || `restinga-node/${version} (https://github.com/jay-ess/restinga-node)`;
 	}
