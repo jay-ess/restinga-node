f33d5df629510bc484bd4baf6980b5f2905046a5
viniazvd <viniazvd@gmail.com>
2017-12-14 11:55:59 -0200

    remove this and return a object.

--- src/descriptor.js
+++ src/descriptor.js
@@ -2,9 +2,11 @@ const debug = require('debug')('restinga:descriptor');
 const {version} = require('../package.json');
 
 module.exports = function ({service, prefix, agent}) {
-	this.service = service;
-	this.prefix = prefix;
-	this.agent = agent || `restinga-node/${version} (https://github.com/jay-ess/restinga-node)`;
-
 	debug(`Creating Descriptor ${service}`);
+
+	return {
+		service,
+		prefix,
+		agent: agent || `restinga-node/${version} (https://github.com/jay-ess/restinga-node)`
+	};
 };
